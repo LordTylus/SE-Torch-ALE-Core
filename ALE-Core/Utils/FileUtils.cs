9eb27b2f4ec78e7e47e78d76384019c6996e34eb
LordTylus <martin.roekker@gmail.com>
2024-11-02 17:48:57 +0100

    Feat: Path Validation for other plugins added

---
+++ ALE-Core/Utils/FileUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace ALE_Core.Utils {
+    public class FileUtils {
+
+        public static string ToValidatedInput(string input) {
+
+            foreach (var c in Path.GetInvalidFileNameChars()) 
+                input = input.Replace(c, '_');
+
+            return input;
+        }
+    }
+}
